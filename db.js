741d5ed258c6867a9cb728b7d7632d36e10865a8
Ramesh Adhikari <adhikari.resume@gmail.com>
2024-12-22 11:33:54 +0530

    add database

--- db.js
+++ db.js
@@ -3,7 +3,7 @@ dbConnect()
 async function dbConnect(){
 
      try {
-         await mongoose.connect('mongodb://localhost:27017/' , {useNewUrlParser : true});
+         await mongoose.connect('mongodb+srv://adhikariresume:THrUIU0gNnoxDduJ@shri-ram.vjyle.mongodb.net/?retryWrites=true&w=majority&appName=shri-ram' , {useNewUrlParser : true});
          console.log('Mongo DB Connection success')
      } catch (error) {
          console.log('Mongo DB Connection failed')
